a0caa14d99b3bb04771b8eef5049d03d12d4192c
임형근 <imhyeong-geun@imhyeong-geun-ui-MacBookAir.local>
2025-10-31 16:55:25 +0900

    fix: add mobile redirect URIs for Kakao login

--- routes/bridge.js
+++ routes/bridge.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/auth', (req, res) => {
+router.get("/auth", (req, res) => {
   const code = req.query.code;
-  if (!code) return res.status(400).send('Missing code');
+  if (!code) return res.status(400).send("Missing code");
 
-  const appRedirect = `exp://172.29.62.22:8081/--/oauth?code=${code}`;
-
-  return res.redirect(appRedirect);
+  const redirect = `https://media.seowon.ac.kr/s202011341/auth?code=${code}`;
+  return res.redirect(redirect);
 });
 
 module.exports = router;
